3d95715a4f017cfcec0514784481388429aaf747
mvalensia <mvalensia@student.ciputra.ac.id>
2026-06-05 20:01:33 +0700

    feat(frontend): add api and auth service

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,56 @@
+import { useState } from "react";
+import { login } from "../services/authService";
+
 function Login() {
-  return <h1>Admin Login</h1>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const data = await login(username, password);
+
+      localStorage.setItem("token", data.token);
+
+      alert("Login berhasil!");
+    } catch (error) {
+      alert("Login gagal!");
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Admin Login</h1>
+
+      <form onSubmit={handleLogin}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <br />
+        <br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <br />
+        <br />
+
+        <button type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
